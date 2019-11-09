51afa25a721cbf49998910433044ae2a709e3d37
dfshapir <das8396@g.harvard.edu>
2019-11-08 20:35:03 -0500

    Very much improved Shiny

--- popshiny/app.R
+++ popshiny/app.R
@@ -4,6 +4,7 @@ library(shinythemes)
 library(plotly)
 
 birthdat <- read_rds("birthdata.rds")
+mortdat <- read_rds("mortdata.rds")
 
 ui <- fluidPage(theme = shinytheme("slate"),
     
@@ -20,14 +21,26 @@ ui <- fluidPage(theme = shinytheme("slate"),
                                selectInput("region", "Region",
                                            choices = levels(birthdat$name),
                                            multiple = TRUE,
-                                           selected = "Republic of North Ossetia-Alania"),
+                                           selected = "Republic of Adygea                 ")
                             ),
                             mainPanel(
                                 plotlyOutput("plotly")
                             )
                         )),
                tabPanel("Mortality Rate",
-                        h5("Text will go here.")),
+                        sidebarLayout(
+                            sidebarPanel(
+                                textInput("titl", "Title", "Regional Mortality Rate by Year, 1992-2010"),
+                                checkboxInput("fi", "Add Line of Best Fit?", value = FALSE),
+                                selectInput("regio", "Region",
+                                            choices = levels(mortdat$name),
+                                            multiple = TRUE,
+                                            selected = "Republic of Adygea                 ")
+                            ),
+                            mainPanel(
+                                plotlyOutput("plotl")
+                            )
+                        )),
                tabPanel("Migration",
                         h5("Text will go here.")))
 )
@@ -38,18 +51,31 @@ server <- function(input, output) {
         data <- subset(birthdat,
                        name %in% input$region)
         
-        p <- ggplot(birthdat, aes(x = year, y = birth, color = name)) + 
+        p <- ggplot(data, aes(x = year, y = birth, color = name)) + 
             geom_point(show.legend = FALSE) + 
             ggtitle(input$title)
         
         ggplotly(p)
         
         if(input$fit == TRUE) {
-            p <- p + geom_smooth(method = "lm")
-        }
-    p
+            p <- p + geom_smooth(method = "lm")}
+            p
+        })
+    output$plotl <- renderPlotly({
+        dat <- subset(mortdat,
+                       name %in% input$region)
+        
+        q <- ggplot(dat, aes(x = year, y = mort, color = name)) + 
+            geom_point(show.legend = FALSE) + 
+            ggtitle(input$titl)
+        
+        ggplotly(q)
+        
+        if(input$fi == TRUE) {
+            q <- q + geom_smooth(method = "lm")}
+        q
     })
-}
+    }
 
 
 shinyApp(ui = ui, server = server)
